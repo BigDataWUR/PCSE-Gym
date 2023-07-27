185b77f72cc9ab91ccf53ad8995d92c5791434a4
Michiel Kallenberg <>
2023-07-27 14:11:35 +0200

    add Ceres to tests

--- tests/test_obs_cost.py
+++ tests/test_obs_cost.py
@@ -1,9 +1,13 @@
 import unittest
-from pcse_gym.envs.winterwheat import WinterWheat
-from pcse_gym.envs.sb3 import *
-from pcse_gym.utils.defaults import *
+import numpy as np
 import time
 import warnings
+import gymnasium as gym
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
+from pcse_gym.envs.winterwheat import WinterWheat
+from pcse_gym.envs.sb3 import get_model_kwargs
+from pcse_gym.utils.defaults import *
+from pcse_gym.utils.eval import FindOptimum
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -136,7 +140,19 @@ class TestRandomFeature(unittest.TestCase):
         self.assertEqual(expected, reward)
 
 
-# TODO add unit test CERES
+class TestCeres(unittest.TestCase):
+    def setUp(self):
+        self.env = initialize_env(pcse_env=0, crop_features=get_crop_features(pcse_env=0))
+        self.env = VecNormalize(DummyVecEnv([lambda: self.env]), norm_reward=True, clip_reward=50., gamma=1)
+
+    def test_single_year(self):
+        ceres_result = FindOptimum(self.env, [1992]).optimize_start_dump().item()
+        self.assertAlmostEqual(17.6, ceres_result, 1)
+
+    def test_multiple_years(self):
+        ceres_result = FindOptimum(self.env, [1992, 2002]).optimize_start_dump().item()
+        self.assertAlmostEqual(19.1, ceres_result, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
