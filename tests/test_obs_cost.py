e71e40a97add1627400e5da295dd194228be036a
Michiel Kallenberg <>
2023-07-27 13:25:15 +0200

    reformatted tests a bit; prep for ceres

--- tests/test_obs_cost.py
+++ tests/test_obs_cost.py
@@ -8,103 +8,89 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def initialize_env():
-    pcse_env = 1
+def get_po_features(pcse_env=1):
     if pcse_env:
         po_features = ['TAGP', 'LAI', 'NAVAIL', 'SM', 'NuptakeTotal']
+    else:
+        po_features = ['TGROWTH', 'LAI', 'TNSOIL', 'NUPTT', 'TRAIN']
+    return po_features
+
+
+def get_crop_features(pcse_env=1):
+    if pcse_env:
         crop_features = get_wofost_default_crop_features()
     else:
-        po_features = ['TGROWTH', 'LAI', 'TNSOIL', 'NUPTT', 'TRAIN']
         crop_features = get_lintul_default_crop_features()
-
-    kwargs = dict(po_features=po_features, args_measure=True)
-
-    a_shape = [7] + [2] * len(po_features)
-    action_spaces = gym.spaces.MultiDiscrete(a_shape)
-
-    env_pcse_eval = WinterWheat(crop_features=crop_features,
-                                action_features=get_default_action_features(),
-                                weather_features=get_default_weather_features(),
-                                costs_nitrogen=10, years=get_default_train_years(),
-                                locations=get_default_location(),
-                                action_space=action_spaces, action_multiplier=1.0, reward='DEF',
-                                **get_model_kwargs(pcse_env), **kwargs)
-
-    return env_pcse_eval
+    return crop_features
 
 
-def initialize_no_baseline():
-    action_spaces = gym.spaces.Discrete(7)
-
-    env_pcse_eval = WinterWheat(crop_features=get_wofost_default_crop_features(),
-                                action_features=get_default_action_features(),
-                                weather_features=get_default_weather_features(),
-                                costs_nitrogen=10, years=get_default_train_years(),
-                                locations=get_default_location(),
-                                action_space=action_spaces, action_multiplier=2.0, reward='GRO',
-                                **get_model_kwargs(1))
-
-    return env_pcse_eval
-
-
-def initialize_random_env():
-    pcse_env = 1
-    if pcse_env:
-        po_features = ['TAGP', 'LAI', 'NAVAIL', 'SM', 'NuptakeTotal', 'random']
-        crop_features = ["DVS", "TAGP", "LAI", "NuptakeTotal", "NAVAIL", "SM"]
-        if 'random' in po_features:
-            crop_features.append('random')
+def get_action_space(nitrogen_levels=7, po_features=[]):
+    if po_features:
+        a_shape = [nitrogen_levels] + [2] * len(po_features)
+        space_return = gym.spaces.MultiDiscrete(a_shape)
     else:
-        po_features = ['TGROWTH', 'LAI', 'TNSOIL', 'NUPTT', 'TRAIN']
-        crop_features = get_lintul_default_crop_features()
-
-    kwargs = dict(po_features=po_features, args_measure=True)
-
-    a_shape = [7] + [2] * len(po_features)
-    action_spaces = gym.spaces.MultiDiscrete(a_shape)
-
-    env_pcse_eval = WinterWheat(crop_features=crop_features,
-                                action_features=get_default_action_features(),
-                                weather_features=get_default_weather_features(),
-                                costs_nitrogen=10, years=get_default_train_years(),
-                                locations=get_default_location(),
-                                action_space=action_spaces, action_multiplier=1.0, reward='DEF',
-                                **get_model_kwargs(pcse_env), **kwargs)
-
-    return env_pcse_eval
+        space_return = gym.spaces.Discrete(nitrogen_levels)
+    return space_return
 
 
-env = initialize_env()
+def initialize_env(pcse_env=1, po_features=[], crop_features=get_crop_features(pcse_env=1),
+                   costs_nitrogen=10, reward='DEF', action_multiplier=1.0, add_random=False):
+    if add_random:
+        po_features.append('random'), crop_features.append('random')
+    action_space = get_action_space(nitrogen_levels=7, po_features=po_features)
+    kwargs = dict(po_features=po_features, args_measure=po_features is not None)
+    env_return = WinterWheat(crop_features=crop_features,
+                             costs_nitrogen=costs_nitrogen,
+                             years=get_default_train_years(),
+                             locations=get_default_location(),
+                             action_space=action_space,
+                             action_multiplier=action_multiplier,
+                             reward=reward,
+                             **get_model_kwargs(pcse_env),
+                             **kwargs)
 
-env2 = initialize_no_baseline()
+    return env_return
 
-env3 = initialize_random_env()
+
+def initialize_env_po():
+    return initialize_env(po_features=get_po_features())
+
+
+def initialize_env_no_baseline():
+    return initialize_env(reward='GRO', action_multiplier=2.0)
+
+
+def initialize_env_random():
+    return initialize_env(po_features=get_po_features(), add_random=True)
+
 
 class TestMeasure(unittest.TestCase):
+    def setUp(self):
+        self.env = initialize_env_po()
 
     def test_oc(self):
-        env.reset()
-        cost = env.measure_features.get_observation_cost()
+        self.env.reset()
+        cost = self.env.measure_features.get_observation_cost()
 
         obs = np.ones(30)
 
         measure = [1, 1, 1, 1, 1]
 
-        _, measurement_cost = env.measure_features.measure_act(obs, measure)
+        _, measurement_cost = self.env.measure_features.measure_act(obs, measure)
 
         self.assertListEqual(cost, list(measurement_cost))
 
     def test_index_match_with_obs(self):
-        env.reset()
+        self.env.reset()
         action = np.array([0, 1, 0, 0, 0, 1])
         # measure = action[1:]
         start = time.time()
-        obs, reward, terminated, truncated, info = env.step(action)
+        obs, reward, terminated, truncated, info = self.env.step(action)
         end = time.time()
-        print(end - start)
+        print(f'time: {end - start}')
         actual = []
         for i in range(1, 4):
-            a = obs[env.measure_features.feature_ind[i]]
+            a = obs[self.env.measure_features.feature_ind[i]]
             actual.append(a)
         expected = list(np.zeros(3))
 
@@ -112,13 +98,15 @@ class TestMeasure(unittest.TestCase):
 
 
 class TestNoMeasure(unittest.TestCase):
+    def setUp(self):
+        self.env = initialize_env_no_baseline()
 
     def test_output(self):
-        env2.reset()
+        self.env.reset()
         action = np.array([3])
-        _, _, _, _, _ = env2.step(action)
+        _, _, _, _, _ = self.env.step(action)
         action = np.array([4])
-        obs, reward, terminated, truncated, info = env2.step(action)
+        obs, reward, terminated, truncated, info = self.env.step(action)
 
         expected = -80
         actual = reward
@@ -127,20 +115,22 @@ class TestNoMeasure(unittest.TestCase):
 
 
 class TestRandomFeature(unittest.TestCase):
+    def setUp(self):
+        self.env = initialize_env_random()
 
     def test_random(self):
-        env3.reset()
+        self.env.reset()
         action = np.array([0, 1, 0, 0, 0, 1, 1])
         measure = action[1:]
-        obs, reward, terminated, truncated, info = env3.step(action)
+        obs, reward, terminated, truncated, info = self.env.step(action)
         expected = []
-        costs = env3.measure_features.get_observation_cost()
+        costs = self.env.measure_features.get_observation_cost()
         for i, cost in zip(measure, costs):
             if i:
                 expected.append(cost)
             else:
                 expected.append(0)
-        #expected = env3.measure_features.list_of_costs()
+        # expected = env3.measure_features.list_of_costs()
         expected = -float(sum(expected))
 
         self.assertEqual(expected, reward)
