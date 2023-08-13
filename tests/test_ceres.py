5edc5c9d1360cf55d32f87f63f5cf9cc21e295e3
Michiel Kallenberg <>
2023-08-13 14:11:54 +0200

    minor fix

--- tests/test_ceres.py
+++ tests/test_ceres.py
@@ -2,11 +2,12 @@ import unittest
 from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 import tests.initialize_env as init_env
 from pcse_gym.utils.eval import FindOptimum
+import pcse_gym.utils.defaults as defaults
 
 
 class TestCeres(unittest.TestCase):
     def setUp(self):
-        self.env = init_env.initialize_env(pcse_env=0, crop_features=init_env.get_crop_features(pcse_env=0))
+        self.env = init_env.initialize_env(pcse_env=0, crop_features=defaults.get_default_crop_features(pcse_env=0))
         self.env = VecNormalize(DummyVecEnv([lambda: self.env]), norm_reward=True, clip_reward=50., gamma=1)
 
     def test_single_year(self):
