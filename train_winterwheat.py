2e00f503e7a61ea2159ffa7277cb10eb32c616c0
hbja <hbja96@gmail.com>
2023-12-13 10:59:20 +0100

    add tag and rename for comet

--- train_winterwheat.py
+++ train_winterwheat.py
@@ -248,7 +248,7 @@ def train(log_dir, n_steps,
         if use_comet:
             comet_log.set_name(f'{cost_measure}-{tag}-{loc_code}-{n_steps}-{agent}-{reward}-{"normalize" if normalize else ""}-'
                                f'{"budget" if n_budget>0 else ""}{"mask_binary" if mask_binary else ""}')
-            comet_log.add_tag(measure_all)
+            comet_log.add_tag(cost_measure)
         tb_log_name = tb_log_name + '-run'
 
         # json_config = get_json_config(n_steps, crop_features, weather_features, train_years, test_years,
